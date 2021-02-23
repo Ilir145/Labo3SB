b70cd691315fab5a3e1466631d543af2fb8c51e8
Ilir145 <70939332+Ilir145@users.noreply.github.com>
2021-02-23 11:45:15 +0100

    Add files via upload

---
+++ labo3SB/src/test/java/be/techni/ilir/labo3SB/Labo3SbApplicationTests.java
@@ -0,0 +1,13 @@
+package be.techni.ilir.labo3SB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Labo3SbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
