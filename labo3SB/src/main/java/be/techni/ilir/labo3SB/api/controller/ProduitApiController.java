7e37ced376fa47d87c9477142f0f6a6d15e6adfa
ilir <ilirbe145@gmail.com>
2021-02-25 11:35:26 +0100

    Api renvois json avec getAll

---
+++ labo3SB/src/main/java/be/techni/ilir/labo3SB/api/controller/ProduitApiController.java
@@ -0,0 +1,48 @@
+package be.techni.ilir.labo3SB.api.controller;
+
+import be.techni.ilir.labo3SB.model.dto.ProduitDTO;
+import be.techni.ilir.labo3SB.model.entities.Produit;
+import be.techni.ilir.labo3SB.model.services.ProduitService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/produits")
+public class ProduitApiController implements RestControllable<Produit, ProduitDTO,Integer> {
+
+    private final ProduitService produitService;
+
+    public ProduitApiController(ProduitService produitService) {
+        this.produitService = produitService;
+    }
+
+    @Override
+    @GetMapping
+    public ResponseEntity<List<ProduitDTO>> getAll() {
+        return ResponseEntity.ok(this.produitService.getAll());
+    }
+
+    @Override
+    public ResponseEntity<ProduitDTO> getOne(Integer integer) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Boolean> insert(Produit produit) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Boolean> update(Produit produit, Integer integer) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Boolean> delete(Integer integer) {
+        return null;
+    }
+}
---
+++ labo3SB/src/main/java/be/techni/ilir/labo3SB/api/controller/RestControllable.java
@@ -0,0 +1,18 @@
+package be.techni.ilir.labo3SB.api.controller;
+
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface RestControllable<ENTITY, DTO, ID> {
+
+    ResponseEntity<List<DTO>> getAll();
+
+    ResponseEntity<DTO> getOne(ID id);
+
+    ResponseEntity<Boolean> insert(ENTITY entity);
+
+    ResponseEntity<Boolean> update(ENTITY entity, ID id);
+
+    ResponseEntity<Boolean> delete(ID id);
+}
